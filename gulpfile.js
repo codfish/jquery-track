e869ee9cb50ed9b99f0fa72f6c2442e3aa77e7fc
Chris O'Donnell <codonnell822@gmail.com>
2017-08-24 12:11:53 -0400

    Remove gulp clean task, add watch

--- gulpfile.js
+++ gulpfile.js
@@ -3,37 +3,23 @@ const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 const sourcemaps = require('gulp-sourcemaps');
-const rimraf = require('rimraf');
-const fs = require('fs');
 const path = require('path');
-
-require('shelljs/global');
 
 
 const paths = {
   src: path.resolve(__dirname, 'src'),
   dist: path.resolve(__dirname, 'dist'),
 };
+const filename = 'jquery.track.js';
 
-
-/**
- * Clean start. Remove all dist files.
- *
- * @see {@link https://www.npmjs.com/package/rimraf}
- */
-gulp.task('clean', function () {
-  rimraf(paths.dist, function (err) {
-    if (err) throw err;
-  });
-});
 
 /**
  * Transpile es6 to es5 using babel
  *
  * @see {@link https://babeljs.io/docs/usage/options/}
  */
-gulp.task('babel', ['clean'], function () {
-  return gulp.src(paths.src + '/**/*.js')
+gulp.task('babel', function () {
+  return gulp.src(`${paths.src}/${filename}`)
     .pipe(sourcemaps.init())
     .pipe(babel())
     .pipe(sourcemaps.write('.'))
@@ -46,7 +32,7 @@ gulp.task('babel', ['clean'], function () {
  * @see {@link https://www.npmjs.com/package/gulp-uglify#options}
  */
 gulp.task('uglify', ['babel'], function () {
-  return gulp.src(paths.dist + '/**/*.js')
+  return gulp.src(`${paths.dist}/${filename}`)
     .pipe(sourcemaps.init())
     .pipe(uglify())
     .pipe(rename({ suffix: '.min' }))
@@ -54,5 +40,10 @@ gulp.task('uglify', ['babel'], function () {
     .pipe(gulp.dest(paths.dist));
 });
 
+gulp.task('watch', () => {
+    gulp.watch('src/*.js', ['babel']);
+});
+
 gulp.task('build', ['uglify']);
+gulp.task('dev', ['build', 'watch']);
 gulp.task('default', ['build']);
